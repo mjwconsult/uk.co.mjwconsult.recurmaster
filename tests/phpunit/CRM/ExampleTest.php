e9f59522ceaca230d6006fdd7aa3a8927425cd59
Matthew Wire <devel@mrwire.co.uk>
2018-03-26 12:28:37 +0100

    Add test framework

---
+++ tests/phpunit/CRM/ExampleTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use CRM_Recurmaster_ExtensionUtil as E;
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
+/**
+ * FIXME - Add test description.
+ *
+ * Tips:
+ *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
+ *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
+ *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
+ *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
+ *    If this test needs to manipulate schema or truncate tables, then either:
+ *       a. Do all that using setupHeadless() and Civi\Test.
+ *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ *
+ * @group headless
+ */
+// FIXME: require trait, should not have to hardcode path like this
+require_once(__DIR__ . '/../../../../org.civicrm.smartdebit/Civi/Test/SmartdebitTestTrait.php');
+
+class CRM_ExampleTest extends \PHPUnit_Framework_TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+
+  use \Civi\Test\Api3TestTrait;
+  use \Civi\Test\SmartdebitTestTrait;
+
+  public function setUpHeadless() {
+    // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+    // See: https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
+    return \Civi\Test::headless()
+      ->install(array('org.civicrm.smartdebit','uk.co.mjwconsult.recurmaster'))
+      ->apply();
+  }
+
+  public function setUp() {
+    parent::setUp();
+    $this->smartdebitPaymentProcessorCreate();
+  }
+
+  public function tearDown() {
+    parent::tearDown();
+  }
+
+  /**
+   * Example: Test that a version is returned.
+   */
+  public function testWellFormedVersion() {
+    $this->assertRegExp('/^([0-9\.]|alpha|beta)*$/', \CRM_Utils_System::version());
+  }
+
+  /**
+   * Example: Test that we're using a fake CMS.
+   */
+  public function testWellFormedUF() {
+    $this->assertEquals('UnitTests', CIVICRM_UF);
+  }
+
+}
